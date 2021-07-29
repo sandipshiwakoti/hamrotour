36d1494a7c66d0d1015c0bc41a32f6d95d04ec34
Sandip Shiwakoti <62270918+sandipshiwakoti@users.noreply.github.com>
2021-07-29 14:24:14 +0545

    Add pure CSS project

--- app.js
+++
@@ -1,9 +0,0 @@
-// navbar fixed top on scroll
-const navbar = document.querySelector("#navbar");
-window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 62) {
-    navbar.classList.add("fixed-top");
-  } else {
-    navbar.classList.remove("fixed-top");
-  }
-});
